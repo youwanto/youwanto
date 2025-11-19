7d307845dd56b6533c2f5b34ada9949bcb6c8a31
Uni <weiqingyu@outlook.com>
2025-11-20 00:10:41 +0100

    fix: no-unescaped-entities

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -15,7 +15,7 @@ const NotFoundPage = () => {
             />
             <div className="p-6 rounded-lg shadow-md w-1/3 text-center">
                 <h1 className="text-3xl font-bold mb-4">404 - Page Not Found</h1>
-                <p className="text-destructive">We're sorry, but the page you were looking for could not be found.</p>
+                <p className="text-destructive">We are sorry, but the page you were looking for could not be found.</p>
                 <Button variant="outline" className="mt-4 ml-2" onClick={() => window.location.href = '/'}>Go to Homepage</Button>
             </div>
         </div>
